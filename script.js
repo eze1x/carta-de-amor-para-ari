8e68738a870db5cbb1e03aeef0c016f61cdbb8aa
eze1x <126312190+eze1x@users.noreply.github.com>
2026-02-12 03:14:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ envelope.addEventListener("click", () => {
 // Función de escritura (compatible con <br>)
 function typeText() {
   let i = 0;
-  const speed = 30;
+  const speed = 50;
 
   function typing() {
     if (i < originalHTML.length) {
@@ -47,3 +47,4 @@ for (let i = 0; i < 25; i++) {
   heart.style.fontSize = 14 + Math.random() * 20 + "px";
   heartsContainer.appendChild(heart);
 }
+
